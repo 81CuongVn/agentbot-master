2fbc207a6ee95dec974189d9de7bad3ffd2c9601
Duy Pham <phamleduy04@gmail.com>
2020-04-15 01:36:08 -0500

    fix bugs

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -2,26 +2,26 @@ module.exports = {
     name: "clear",
     aliases: ["purge"],
     category: "moderation",
-    description: "Clears the chat",
+    description: "Xoá tin nhắn",
     run: async(client, message, args) => {
-        message.delete();
+        await message.delete();
 
         // Member doesn't have permissions
         if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-            return message.reply("Mày đéo có quyền Manage Message nên tao đéo xoá").then(m => m.delete(5000));
+            return message.reply("Bạn không có quyền MANAGE_MESSAGES").then(m => m.delete({timeout: 5000}));
         }
 
         // Check if args[0] is a number
         if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
-            return message.reply("Nhập số nào tao có thể xoá đi dmm").then(m => m.delete(5000));
+            return message.reply("Vui lòng nhập số để bot có thể xoá").then(m => m.delete({timeout: 5000}));
         }
 
         // Maybe the bot can't delete messages
         if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
-            return message.reply("Tao k có quyền Manage Message nên tao k xoá được").then(m => m.delete(5000));
+            return message.reply("Bot không có quyền MMANAGE_MESSAGES nên bot không thể xoá.").then(m => m.delete({timeout: 5000}));
         }
 
-        let deleteAmount;
+        let deleteAmount = 0;
 
         if (parseInt(args[0]) > 100) {
             deleteAmount = 100;
@@ -30,6 +30,6 @@ module.exports = {
         }
 
         message.channel.bulkDelete(deleteAmount, true)
-            .then(deleted => message.channel.send(`Đã xoá \`${deleted.size}\` messages.`)).then(m => m.delete())
+            .then(deleted => message.channel.send(`Đã xoá \`${deleted.size}\` messages.`)).then(m => m.delete({timeout: {timeout: 5000}}))
     }
 }
