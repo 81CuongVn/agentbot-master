28f73a4f86b35e1e23e3f448f9758528fb0f2de5
Duy Pham <phamleduy04@gmail.com>
2020-04-15 16:46:47 -0500

    fix bugs + add slap.js

--- commands/images/slap.js
+++ commands/images/slap.js
@@ -1,28 +1,26 @@
 const { MessageEmbed} = require("discord.js");
 const getJSON = require("get-json");
-const fs = require('fs')
-const url = 'https://getrandompic.herokuapp.com/randompunch'
+const url = 'https://getrandompic.herokuapp.com/randomslap'
 module.exports = {
-    name: "punch",
+    name: "slap",
     category: "images",
-    description: "Punch someone :D ",
-    usage: "_punch <@tag>",
+    description: "Tát ai đó",
+    usage: "_slap <@tag>",
     run: (client, message, args) => {
         getJSON(url, function(error, response){
             if (error) return message.channel.send('Bot lỗi trong khi lấy hình, vui lòng thử lại sau.')
             let nguoitag = message.mentions.members.array() || message.guild.members.get(args[0])
             if (nguoitag.length == 0) {
                 const embed1 = new MessageEmbed()
-                    .setDescription(`${message.member} đã tự đấm chính mình 👊`)
+                    .setDescription(`${message.member} đã tự vả chính mình 🤚`)
                     .setImage(response.link)
                 return message.channel.send(embed1)
             } else {
                 const embed = new MessageEmbed()
-                    .setDescription(`${message.member} đã đấm vỡ mồm 🤜 ${nguoitag}`)
+                    .setDescription(`${message.member} đã tát vỡ mồm ${nguoitag} 🤚`)
                     .setImage(response.link)
                 return message.channel.send(embed)
                 }
         })
-        
     }
 }
