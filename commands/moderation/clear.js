1411da45166e84006d264cb3d4691c5b40d5d50a
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-05-01 04:17:26 -0500

    Nâng cấp lệnh clear

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -1,9 +1,11 @@
+const db = require('quick.db');
 module.exports = {
     name: "clear",
     aliases: ["purge"],
     category: "moderation",
     description: "Xoá tin nhắn",
-    usage: "clear <số tin nhắn>",
+    usage: "clear [@tag] <số tin nhắn>",
+    VD: "clear @phamleduy04 10",
     run: async(client, message, args) => {
         await message.delete();
 
@@ -12,25 +14,31 @@ module.exports = {
             return message.reply("Bạn không có quyền MANAGE_MESSAGES").then(m => m.delete({timeout: 5000}));
         }
 
-        // Check if args[0] is a number
-        if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
-            return message.reply("Vui lòng nhập số để bot có thể xoá").then(m => m.delete({timeout: 5000}));
-        }
-
         // Maybe the bot can't delete messages
         if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
             return message.reply("Bot không có quyền MMANAGE_MESSAGES nên bot không thể xoá.").then(m => m.delete({timeout: 5000}));
         }
-
-        let deleteAmount = 0;
-
-        if (parseInt(args[0]) > 100) {
-            deleteAmount = 100;
+        const user = message.mentions.users.first()
+        const ammount = !!parseInt(args[0]) ? parseInt(args[0]) : parseInt(args[1])
+        if (!ammount) return message.reply('Vui lòng nhập số lượng tin nhắn để xoá.')
+        if (ammount < 1) return message.reply('Vui lòng nhập số lớn hơn 1.')
+        if (ammount > 100) return message.reply('Vui lòng nhập số nhỏ hơn 100.')
+        if (!ammount && !user) return message.channel.send(`Sử dụng lệnh \`${db.get(`${message.guild.id}.prefix`)}help clear\` để biết thêm thông tin.`)
+        if (!user) {
+            message.channel.bulkDelete(ammount, true).then(delmsg => {
+                message.channel.send(`Đã xoá \`${delmsg.size}\` tin nhắn!`).then(m => m.delete({timeout: 5000}))
+            })
         } else {
-            deleteAmount = parseInt(args[0]);
+            message.channel.messages.fetch({
+                limit: 100,
+            }).then(messages => {
+                messages = messages.filter(m => m.author.id === user.id).array().slice(0, ammount)
+                message.channel.bulkDelete(messages, true).then(delmsg => {
+                    message.channel.send(`Đã xoá \`${delmsg.size}\` tin nhắn!`).then(m => m.delete({timeout: 5000}))
+                })
+            }) 
         }
-
-        message.channel.bulkDelete(deleteAmount, true)
-            .then(deleted => message.channel.send(`Đã xoá **${deleted.size}** messages.`)).then(m => m.delete({timeout: 5000}))
+        
     }
+
 }
