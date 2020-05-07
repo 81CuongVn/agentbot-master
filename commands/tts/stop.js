fcd1bb3d411ce570093f56f18aeb1147e83304a1
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-05-07 17:57:48 -0500

    Fix bugs

--- commands/tts/stop.js
+++ commands/tts/stop.js
@@ -10,8 +10,8 @@ module.exports = {
         let voiceChannel = message.member.voice.channel;
         if (message.author.id !== ownerID && !voiceChannel) return message.channel.send('Bạn phải vào voice mới có thể sử dụng lệnh này!')
         if (!voiceChannel.members.get(client.user.id)) return message.channel.send('Bot không ở chung phòng với bạn!')
-        let voiceConnection = message.guild.me.voice.connection
-        await voiceConnection.disconnect()
+        let dispatcher = message.guild.me.voice.connection.dispatcher
+        await dispatcher.pause()
         message.react('✅')
         await db.set(`${message.guild.id}.botdangnoi`, false)
     }
