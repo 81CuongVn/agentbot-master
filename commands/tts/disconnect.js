7addefe78a92e6ad9bce7c32dd558f5d50573d97
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-05-07 22:46:58 -0500

    Fix bugs

--- commands/tts/disconnect.js
+++ commands/tts/disconnect.js
@@ -2,14 +2,14 @@ const {ownerID} = require('../../config.json');
 const db = require('quick.db');
 module.exports = {
     name: 'disconnect',
-    aliases: ['leave'],
+    aliases: ['leave', 'dis'],
     category: 'tts',
     description: 'Để disconnect cho bot',
     usage: 'disconnect',
     VD: 'disconnect',
     run: async(client, message, args) => {
         let voiceChannel = message.member.voice.channel;
-        if (message.author.id !== ownerID && !voiceChannel) return message.channel.send('Bạn phải vào voice mới có thể sử dụng lệnh này!')
+        if (!voiceChannel) return message.channel.send('Bạn phải vào voice mới có thể sử dụng lệnh này!')
         if (!voiceChannel.members.get(client.user.id)) return message.channel.send('Bot không ở chung phòng với bạn!')
         await db.set(`${message.guild.id}.botdangnoi`, false)
         await voiceChannel.leave();
