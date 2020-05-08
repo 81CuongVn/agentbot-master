fd1aebe288ade23f03a64553b8bee4cd086abce2
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-05-08 18:14:36 -0500

    Fix ask.js

--- commands/fun/ask.js
+++ commands/fun/ask.js
@@ -1,11 +1,11 @@
-var fs = require('fs')
-var array = fs.readFileSync('././ask.txt', 'utf8').split('\r\n')
+const askdata = require('../../ask.json')
 module.exports = {
     name: "ask",
     category: "fun",
     description: "Trả về câu trả lời đúng, sai hoặc không biết.",
     note: "Hơi gắt xíu D:",
     run: async(client, message, args) => {
+        let array = askdata.askdb
         if (!args[0]) return message.reply("Hỏi gì đi chứ bạn :D.")
         const random = array[Math.floor(Math.random() * array.length)];
         return message.reply(random);
