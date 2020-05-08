d514c0c383c091c05e4bde9875d17f0f3fd43416
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-05-08 18:02:32 -0500

    Update time.js

---
+++ commands/info/time.js
@@ -0,0 +1,26 @@
+const getJSON = require('get-json');
+const uri = require('strict-uri-encode');
+const {gggeolocaionkey, timezonedb} = require('../../config.json');
+module.exports = {
+    name: 'time',
+    category: 'info',
+    description: 'tìm giờ bằng cách nhập tên TP',
+    usage: 'time <nhập địa chỉ, zipcode, hay gì cũng được>',
+    VD: 'time Dallas,TX',
+    run: async (client, message, args) => {
+        if (!args[0]) return message.reply('Ghi tên thành phố hoặc địa chỉ của bạn!.')
+        let search = args.join(' ')
+        search = uri(search)
+        getJSON(`https://maps.googleapis.com/maps/api/geocode/json?address=${search}&key=${gggeolocaionkey}`, function(error, response){
+            if (error) return message.channel.send('Bot lỗi, vui lòng thử lại sau!')
+            let res = response.results[0]
+            if (res.geometry) {
+                let timeurl = `http://api.timezonedb.com/v2.1/get-time-zone?key=${timezonedb}&format=json&by=position&lat=${res.geometry.location.lat}&lng=${res.geometry.location.lng}`
+                getJSON(timeurl, function(error, response){
+                    if (error) return message.channel.send('Bot lỗi, vui lòng thử lại sau!')
+                    message.channel.send(`Múi giờ ${response.zoneName}, ${response.formatted}`)
+                })
+            }
+        })
+    }
+}
