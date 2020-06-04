1a9b6a8b41ef1dc5aedeca9d9f30739d37dce675
Duy Pham Le <phamleduy04@gmail.com>
2020-06-03 21:15:51 -0500

    upload

--- commands/info/instagram.js
+++ commands/info/instagram.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 const { stripIndents } = require("common-tags");
-const axios = require('axios');
+
+const fetch = require("node-fetch");
 
 module.exports = {
     name: "instagram",
@@ -15,27 +16,31 @@ module.exports = {
 
 
         const url = `https://instagram.com/${name}/?__a=1`;
-        axios.get(url)
-            .then(function(response){
-                const account = response.data.graphql.user;
 
-                const embed = new MessageEmbed()
-                    .setColor("RANDOM")
-                    .setTitle(account.full_name)
-                    .setURL(`https://instagram.com/${name}`)
-                    .setThumbnail(account.profile_pic_url_hd)
-                    .addField("Thông tin cá nhân", stripIndents `**- Tên người dùng:** ${account.username}
-                    **- Tên đầy đủ:** ${account.full_name}
-                    **- Bio:** ${account.biography.length == 0 ? "Không có" : account.biography}
-                    **- Số bài đăng:** ${account.edge_owner_to_timeline_media.count}
-                    **- Followers:** ${account.edge_followed_by.count}
-                    **- Following:** ${account.edge_follow.count}
-                    **- Private?:** ${account.is_private ? "Có 🔐" : "Không 🔓"}`);
+        let res;
 
-                message.channel.send(embed);
-            })
-            .catch(function(error){
-                return message.channel.send('Bot không tìm thấy bạn!, vui lòng thử lại sau.')
-            })
+        try {
+            res = await fetch(url).then(url => url.json());
+        } catch (e) {
+            return message.reply("Không tìm thấy account Instagram, bạn hãy kiểm tra lại giúp mình nha")
+                .then(m => m.delete({timeout: 5000}));
+        }
+
+        const account = res.graphql.user;
+
+        const embed = new MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle(account.full_name)
+            .setURL(`https://instagram.com/${name}`)
+            .setThumbnail(account.profile_pic_url_hd)
+            .addField("Thông tin cá nhân", stripIndents `**- Tên người dùng:** ${account.username}
+            **- Tên đầy đủ:** ${account.full_name}
+            **- Bio:** ${account.biography.length == 0 ? "Không có" : account.biography}
+            **- Số bài đăng:** ${account.edge_owner_to_timeline_media.count}
+            **- Followers:** ${account.edge_followed_by.count}
+            **- Following:** ${account.edge_follow.count}
+            **- Private?:** ${account.is_private ? "Có 🔐" : "Không 🔓"}`);
+
+        message.channel.send(embed);
     }
 }
