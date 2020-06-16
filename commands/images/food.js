4154d1a318f5d463524a4f14bfd17031c1a306bc
Duy Pham Le <phamleduy04@gmail.com>
2020-06-16 16:33:17 -0500

    update food.js + remove random-puppy

--- commands/images/food.js
+++ commands/images/food.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require("discord.js");
-const randomPuppy = require("random-puppy");
-
+const { KSoftClient } = require('ksoft.js');
+var { ksoft_key } = require('../../config.json')
+const ksoft = new KSoftClient(ksoft_key);
 module.exports = {
     name: "food",
     category: "images",
@@ -9,14 +10,18 @@ module.exports = {
     run: async(client, message, args) => {
         const subReddits = ["appetizers", "asianeats", "BBQ", "bento", "BreakfastFood", "burgers", "cakewin", "Canning", "cereal", "charcuterie", "Cheese", "chinesefood", "cider", "condiments", "curry", "culinaryplating", "cookingforbeginners", "cookingwithcondiments", "doener", "eatwraps", "fastfood", "fishtew", "fried", "GifRecipes", "grease", "hot_dog", "icecreamery", "irish_food", "JapaneseFood", "jello", "KoreanFood", "FoodPorn", "meat", "pasta", "pizza", "ramen", "seafood", "spicy", "steak", "sushi", "sushiroll", 'Vitamix'];
         const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-
-        const img = await randomPuppy(random);
-        const embed = new MessageEmbed()
-            .setColor("RANDOM")
-            .setImage(img)
-            .setTitle(`From /r/${random}`)
-            .setURL(`https://reddit.com/r/${random}`);
-
-        message.channel.send(embed);
+        try {
+            const img = await ksoft.images.reddit(random, {removeNSFW: true, span: 'week'})
+            let embed = new MessageEmbed()
+                .setColor('RANDOM')
+                .setImage(img.url)
+                .setTitle(`Từ /r/${random}`)
+                .setURL(img.post.link)
+                .setFooter(`Upvote: ${img.post.upvotes} | Downvote: ${img.post.downvotes}`)
+            message.channel.send(embed)
+        } catch(e) {
+            message.channel.send('Bot lỗi, vui lòng thử lại sau!')
+            console.error(e)
+        }
     }
 }
