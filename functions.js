7e0c424abb5dccb409e8f15d3f1bbd220523f30c
Duy Pham Le <phamleduy04@gmail.com>
2020-06-21 18:58:39 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -176,12 +176,18 @@ module.exports = {
         }
         return true
     },
-    trimArray(arr, maxLen) {
+    trimArray: function(arr, maxLen) {
         if (arr.length > maxLen) {
             const len = arr.length - maxLen
             arr = arr.slice(0, maxLen);
             arr.push(`${len} more....`);
         }
         return arr;
+    },
+    formatBytes: function(bytes){
+        if (bytes === 0) return '0 Bytes';
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
+        const i = Math.floor(Math.log(bytes) / Math.log(1024));
+        return `${parseFloat((bytes / Math.pow(1024, i)).toFixed(2))} ${sizes[i]}`
     }
 }
