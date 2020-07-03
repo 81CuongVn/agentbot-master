41cb85e25d254ec35587ad3906e25c1749d2e523
Duy Pham Le <phamleduy04@gmail.com>
2020-07-03 00:13:57 -0500

    update whois command + fix serverinfo command

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -1,6 +1,5 @@
 const serverFlags = require('../../data/serverflag.json');
 const { MessageEmbed } = require('discord.js');
-const { stripIndent } = require('common-tags');
 const { trimArray } = require('../../functions');
 const moment = require('moment');
 module.exports = {
@@ -19,23 +18,21 @@ module.exports = {
             .setColor('BLUE')
             .setFooter(`Server ID: ${message.guild.id}`)
             .setThumbnail(message.guild.iconURL({ dynamic: true }))
-            .addField('Chung', stripIndent`**--> Tên server:** ${message.guild.name}
-        **--> Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})
-        **--> Level của server:** ${message.guild.premiumTier ? `Level ${message.guild.premiumTier}` : 'None'}
-        **--> Bộ lọc:** ${serverFlags.filterLevels[message.guild.explicitContentFilter]}
-        **--> Mức độ xác minh:** ${serverFlags.verificationLevels[message.guild.verificationLevel]}`)
-            .addField('Thống kê', stripIndent`**--> Số role:** ${roles.length}
-        **--> Số emoji:** ${emojis.size} (${emojis.filter(e => !e.animated).size} emoji thường và ${emojis.filter(e => e.animated).size} emoji động)
-        **--> Thành viên:** ${message.guild.memberCount} (${members.filter(m => !m.user.bot).size} người và ${members.filter(m => m.user.bot).size} bot)
-        **--> Channel:** ${channels.filter(c => c.type === 'text').size + channels.filter(c => c.type === 'voice').size} (${channels.filter(c => c.type === 'text').size} text channel và ${channels.filter(c => c.type === 'voice').size} voice channel)
-        **--> Số boost:** ${message.guild.premiumSubscriptionCount || '0'}
-        **--> Ngày tạo server:** ${moment(message.guild.createdTimestamp).format('MM/DD/YYYY hh:mm:ss')}
-        `)
-            .addField('Thống kê thành viên', stripIndent`**--> Online:** ${members.filter(m => m.presence.status === 'online').size} 
-        **--> Không hoạt động:** ${members.filter(m => m.presence.status === 'idle').size}
-        **--> Không làm phiền:** ${members.filter(m => m.presence.status === 'dnd').size}
-        **--> Offline:** ${members.filter(m => m.presence.status === 'offline').size}
-        `)
+            .addField('Chung', [`**--> Tên server:** ${message.guild.name}`,
+        `**--> Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
+        `**--> Level của server:** ${message.guild.premiumTier ? `Level ${message.guild.premiumTier}` : 'None'}`,
+        `**--> Bộ lọc:** ${serverFlags.filterLevels[message.guild.explicitContentFilter]}`,
+        `**--> Mức độ xác minh:** ${serverFlags.verificationLevels[message.guild.verificationLevel]}`])
+            .addField('Thống kê', [`**--> Số role:** ${roles.length}`,
+        `**--> Số emoji:** ${emojis.size} (${emojis.filter(e => !e.animated).size} emoji thường và ${emojis.filter(e => e.animated).size} emoji động)`,
+        `**--> Thành viên:** ${message.guild.memberCount} (${members.filter(m => !m.user.bot).size} người và ${members.filter(m => m.user.bot).size} bot)`,
+        `**--> Channel:** ${channels.filter(c => c.type === 'text').size + channels.filter(c => c.type === 'voice').size} (${channels.filter(c => c.type === 'text').size} text channel và ${channels.filter(c => c.type === 'voice').size} voice channel)`,
+        `**--> Số boost:** ${message.guild.premiumSubscriptionCount || '0'}`,
+        `**--> Ngày tạo server:** ${moment(message.guild.createdTimestamp).format('MM/DD/YYYY hh:mm:ss')}`])
+            .addField('Thống kê thành viên', [`**--> Online:** ${members.filter(m => m.presence.status === 'online').size}`,
+        `**--> Không hoạt động:** ${members.filter(m => m.presence.status === 'idle').size}`,
+        `**--> Không làm phiền:** ${members.filter(m => m.presence.status === 'dnd').size}`,
+        `**--> Offline:** ${members.filter(m => m.presence.status === 'offline').size}`])
             .addField(`Roles: `, roles.length < 10 ? roles.join(', ') : roles.length > 10 ? trimArray(roles, 10) : 'None')
             .setTimestamp()
         message.channel.send(embed)
--- commands/info/whois.js
+++ commands/info/whois.js
@@ -1,14 +1,14 @@
 const { MessageEmbed } = require("discord.js");
 const { stripIndents } = require("common-tags");
-const { getMember, formatDate, trimArray} = require("../../functions.js");
-
+const { getMember, formatDate, trimArray } = require("../../functions.js");
+const flags = require('../../data/userflag.json');
 module.exports = {
     name: "whois",
     aliases: ["who", "user", "info"],
     category: "info",
     description: "Returns user information",
     usage: "whois <tag,username,ID>",
-    run: (client, message, args) => {
+    run: async (client, message, args) => {
         const member = getMember(message, args.join(" "));
             // Member variables
         const joined = formatDate(member.joinedAt);
@@ -19,25 +19,25 @@ module.exports = {
         roles.join(", ") || 'none';
         // User variables
         const created = formatDate(member.user.createdAt);
-
+        const userFlags = member.user.flags.toArray()
         const embed = new MessageEmbed()
             .setFooter(member.displayName, member.user.avatarURL())
             .setThumbnail(member.user.avatarURL())
             .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
 
-        .addField('Member information:', stripIndents `**- Display name:** ${member.displayName}
-            **- Joined at:** ${joined}
+        .addField('Thông tin thành viên (server):', stripIndents `**- Nickname:** ${member.displayName}
+            **- Vào server vào ngày:** ${joined}
             **- Roles:** ${roles.length < 10 ? roles.join(', ') : roles.length > 10 ? trimArray(roles, 10) : 'None'}`, true)
 
-        .addField('User information:', stripIndents `**- ID:** ${member.user.id}
-            **- Username**: ${member.user.username}
+        .addField('Thông tin người dùng:', stripIndents `**- ID:** ${member.user.id}
+            **- Tên người dùng**: ${member.user.username}
             **- Tag**: ${member.user.tag}
-            **- Created at**: ${created}`, true)
+            **- Tạo vào lúc**: ${created}
+            **- Huy hiệu**: ${userFlags.length ? userFlags.map(flag => flags[flag]).join(", ") : "Không có" }`, true)
 
         .setTimestamp()
-
-        if (member.user.presence.game)
-            embed.addField('Currently playing', stripIndents `** Name:** ${member.user.presence.game.name}`);
+        if (member.user.presence.activities.length > 0)
+            embed.addField('Đang chơi: ', stripIndents `** Tên game:** ${member.user.presence.activities[0].name}`);
 
         message.channel.send(embed);
     }
