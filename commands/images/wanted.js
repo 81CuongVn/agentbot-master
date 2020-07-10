827abbd1e24721027ad0b02d6d59e5ee6209e76a
Duy Pham Le <phamleduy04@gmail.com>
2020-07-10 01:36:18 -0500

    Create wanted.js

---
+++ commands/images/wanted.js
@@ -0,0 +1,13 @@
+const Canvacord = require('canvacord');
+const canva = new Canvacord.Canvas();
+const { MessageAttachment } = require('discord.js');
+module.exports = {
+    name: 'wanted',
+    aliases: ['truyna'],
+    category: 'images',
+    description: 'Truy nã',
+    usage: 'truyna',
+    run: async (client, message, args) => {
+        
+    }
+}
