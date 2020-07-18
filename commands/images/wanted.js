a2cefb56838687b9c6b9525f6a3c66b79e7b2096
Duy Pham Le <phamleduy04@gmail.com>
2020-07-17 20:43:27 -0500

    fix bugs

--- commands/images/wanted.js
+++ commands/images/wanted.js
@@ -1,5 +1,5 @@
 const Canvacord = require('canvacord');
-const canva = new Canvacord.Canvas();
+const canva = new Canvacord();
 const { MessageAttachment } = require('discord.js');
 module.exports = {
     name: 'wanted',
@@ -8,6 +8,10 @@ module.exports = {
     description: 'Truy nã',
     usage: 'truyna',
     run: async (client, message, args) => {
-        
+        let nguoitag = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member
+        let avaurl = nguoitag.user.avatarURL({ format: 'png', dynamic: false })
+        let image = await canva.wanted(avaurl);
+        let attach = new MessageAttachment(image, 'wanted.png')
+        return message.channel.send(attach)
     }
 }
