e2a86a2c87ec49902d35c6205b289b9ee2b61faf
Duy Pham Le <phamleduy04@gmail.com>
2020-07-25 15:14:39 -0500

    upload new files

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,11 +1,15 @@
 var getJSON = require("get-json");
 const { MessageEmbed } = require('discord.js');
+const publicIP = require('public-ip');
+const iplocation = require('iplocation');
 module.exports = {
     name: "ping",
     category: "info",
     description: "Returns latency and API ping",
     usage: "ping",
     run: async(client, message, args) => {
+        let myIP = await publicIP.v4();
+        let data = await iplocation(myIP);
         let url = `https://srhpyqt94yxb.statuspage.io/api/v2/components.json`
         getJSON(url, async function(error, response) {
             if (error) return message.channel.send('Bot gặp lỗi trong khi cố gắng kết nối tới Discord')
@@ -15,6 +19,7 @@ module.exports = {
             const embed = new MessageEmbed()
                 .setDescription(`Độ trễ là: ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms`)
                 .addField('Discord API status: ', api_json.status, true)
+                .addField('Vị trí hosting: ', `${data.city}, ${data.region.code}, ${data.country.name}`)
             await msg.delete();
             message.channel.send(embed)
         })
