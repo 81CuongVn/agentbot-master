222a384da1b782e3281a72c324014305363f7ad3
Duy Pham Le <phamleduy04@gmail.com>
2020-07-31 21:07:18 -0500

    upload files

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,7 +1,8 @@
 var getJSON = require("get-json");
 const { MessageEmbed } = require('discord.js');
 const publicIP = require('public-ip');
-const iplocation = require('iplocation');
+const { ipgeolocation } = require('../../config.json');
+const axios = require('axios');
 module.exports = {
     name: "ping",
     category: "info",
@@ -9,7 +10,8 @@ module.exports = {
     usage: "ping",
     run: async(client, message, args) => {
         let myIP = await publicIP.v4();
-        let data = await iplocation(myIP);
+        let data = await axios.get(`https://api.ipgeolocation.io/ipgeo?apiKey=${ipgeolocation}&ip=${myIP}`);
+        data = data.data;
         let url = `https://srhpyqt94yxb.statuspage.io/api/v2/components.json`
         getJSON(url, async function(error, response) {
             if (error) return message.channel.send('Bot gặp lỗi trong khi cố gắng kết nối tới Discord')
@@ -19,7 +21,7 @@ module.exports = {
             const embed = new MessageEmbed()
                 .setDescription(`Độ trễ là: ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms`)
                 .addField('Discord API status: ', api_json.status, true)
-                .addField('Vị trí hosting: ', `${data.city}, ${data.region.code}, ${data.country.name}`)
+                .addField('Vị trí hosting: ', `${data.city}, ${data.state_prov}, ${data.country_code2}`)
             await msg.delete();
             message.channel.send(embed)
         })
