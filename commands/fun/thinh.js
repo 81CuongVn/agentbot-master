83034111d3980f42909f3497cd4f3e6bdb91cbaa
Duy Pham Le <phamleduy04@gmail.com>
2020-08-10 15:19:43 -0500

    update thính

--- commands/fun/thinh.js
+++ commands/fun/thinh.js
@@ -1,11 +1,11 @@
-const fs = require('fs')
-const array = fs.readFileSync('./data/thinh.txt', 'utf8').split('\n')
+const array = require('../../data/thinh.json').data;
 module.exports = {
     name: "thinh",
     aliases: ["xinthinh"],
     category: "fun",
     description: "Xin thính",
     usage: "thinh",
+    cooldown: 2,
     run: async (client, message, args) => {
         let random = array[Math.floor(Math.random() * array.length)]
         message.channel.send(random)
